e3c5c5b79becb937c1c47691dd0ebccaa0ba6edc
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2024-03-10 17:34:31 +0900

    test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
